99f40097fcdbbb810f3712d582b406a22c111de6
Menghan <menghan@douban.com>
2014-11-04 15:42:07 +0800

    use tree data structure to avoid inefficient Vector copy

---
+++ src/main/java/com/huaban/analysis/jieba/Node.java
@@ -0,0 +1,11 @@
+package com.huaban.analysis.jieba;
+
+public class Node {
+    public Character value;
+    public Node parent;
+
+    public Node(Character value, Node parent) {
+        this.value = value;
+        this.parent = parent;
+    }
+}
